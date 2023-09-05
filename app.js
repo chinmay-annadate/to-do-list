ff6e5f8a25923fee314935bd7edd5dc02d8929da
chinmay.annadate <ccannadate@gmail.com>
2023-09-05 23:17:26 +0530

    comments

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
-let tasks = [];
-
 const app = express();
 
 app.set("view engine", "ejs");
@@ -12,14 +10,18 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// change localhost to 127.0.0.1 or won't work
 mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
 
+// schema
 const itemSchema = {
   name: String,
 };
 
+// model
 const Item = mongoose.model("Item", itemSchema);
 
+// retreive data and render
 app.get("/", function (req, res) {
   let day = date();
   Item.find({}).then((data) => {
@@ -27,11 +29,13 @@ app.get("/", function (req, res) {
   });
 });
 
+// store new item
 app.post("/", function (req, res) {
   Item.insertMany([new Item({ name: req.body.task })]);
   res.redirect("/");
 });
 
+// delete checked item
 app.post("/delete", function (req, res) {
   Item.findByIdAndDelete(req.body.checkbox).then(res.redirect("/"));
 });
